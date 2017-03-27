40f838eb5ff454922e3d3e3d5fd1f23559baea17
Johnsonlee <johnsonlee28@gmail.com>
2017-03-27 16:49:18 +0800

    add devise and auth_token

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, :defaults => { :format => :json} do
+    namespace :v1 do
+      get "/trains" => "trains#index", :as => :trains
+      get "/trains/:train_number" => "trains#show", :as => :train
+
+      get "/reservations/:booking_code" => "reservations#show", :as => :reservation
+      post "/reservations" => "reservations#create", :as => :create_reservations
+      patch "/reservations/:booking_code" => "reservations#update", :as => :update_reservation
+      delete "/reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
+      get "/reservations" => "reservations#index", :as => :reservations
+    end
+  end
+  root "welcome#index"
+
   resources :cities do
     member do
       post :update_temp
